10eb2fc9f84992d2d5c87a4f3d81f14760d051a9
halfapencil <tonylin1524@gmail.com>
2024-11-15 02:05:13 -0500

    more base code to be built on later

--- Processing.java
+++ Processing.java
@@ -1,13 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Processing {
     ArrayList<Student> studentList = new ArrayList<Student>();
 
-    public Processing() {
-        fileInput();
+    public Processing(File course, File name) {
+        try {
+            Scanner courseScanner = new Scanner(course);
+            Scanner nameScanner = new Scanner(name);
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        fileInput(course, name);
     }
 
-    private void fileInput() {
+    private void fileInput(File course, File name) {
 
     }
 
--- Student.java
+++ Student.java
@@ -3,5 +3,18 @@ public class Student {
     private String name;
     private float grade;
     private String sId;
+    private String courseId;
+
+    public Student(String name, String sId, String courseId, float[] grades) {
+        this.name = name;
+        this.sId = sId;
+        this.courseId = courseId;
+        this.grade = calculate(grades);
+    }
+
+    private double calculate(double[] grades) {
+
+        return (grades[0] * 0.2) + (grades[1] * 0.2) + (grades[2] * 0.2) + (grades[3] * 0.4);
+    }
 
 }
